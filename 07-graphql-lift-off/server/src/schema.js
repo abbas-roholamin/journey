84e8e13de53d905ec3b229dfd415e01e6cb8ce50
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-08 09:40:45 +0430

    [feat]: Implement resolver chain

--- 07-graphql-lift-off/server/src/schema.js
+++ 07-graphql-lift-off/server/src/schema.js
@@ -4,6 +4,8 @@ const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -14,6 +16,7 @@ const typeDefs = gql`
     thumbnail: String
     length: Int
     modulesCount: Int
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -21,6 +24,14 @@ const typeDefs = gql`
     id: ID!
     name: String!
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    title: String!
+    length: Int
+    content: String
   }
 `;
 
