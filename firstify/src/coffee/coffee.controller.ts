a036402e09cfbfa95bb477bab87402df29ecf905
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2024-12-15 23:05:53 +0430

    feat(firstify): Implement Pagination with Query Parameters

--- firstify/src/coffee/coffee.controller.ts
+++ firstify/src/coffee/coffee.controller.ts
@@ -8,16 +8,18 @@ import {
   Param,
   Post,
   Put,
+  Query,
   //   Res,
 } from '@nestjs/common';
 
 @Controller('coffee')
 export class CoffeeController {
   @Get()
-  findAll() {
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
     // findAll(@Res() res) {
     // res.status(200).send('Get All Coffees');
-    return 'Get All Coffees';
+    return `Get All Coffees with limit ${limit} and offset ${offset}`;
   }
 
   @Get(':id')
