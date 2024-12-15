937f954dbb42c1adcc205458cdd9a3f1c64a3487
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2024-12-15 22:43:53 +0430

    feat(firstify): Use Route Parameters

--- firstify/src/coffee/coffee.controller.ts
+++ firstify/src/coffee/coffee.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffee')
 export class CoffeeController {
   @Get()
   findAll() {
     return 'Get All Coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Get coffee ${id}`;
   }
 }
