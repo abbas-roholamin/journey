5420eea04ab0260bf732071e0c6e3add24791268
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2024-12-15 20:46:39 +0430

    feat(prisma): Create get all user endpoint

--- 03-prisma/src/index.ts
+++ 03-prisma/src/index.ts
@@ -1,1 +1,34 @@
+import { PrismaClient } from '@prisma/client'
+import http from 'http'
+const prisma = new PrismaClient()
+
+async function main() {
+    const server = http.createServer(async (req, res) => {
+        // get all users
+        if (req.url === '/users' && req.method === 'GET') {
+            const users = await prisma.user.findMany()
+
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(users))
+
+            // 404
+        } else {
+            res.writeHead(404)
+            res.end()
+        }
+    })
+
+    server.listen(8000, '127.0.0.1', () => {
+        console.log('Listening on http://127.0.0.1:8000')
+    })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
     })
