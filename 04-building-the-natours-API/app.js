64be3a4a13d1ee67e6507b78e91b9cdd8bc229f3
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-16 09:48:33 +0430

    feat(natours): Create get all tours endpoint

--- 04-building-the-natours-API/app.js
+++ 04-building-the-natours-API/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours,
+        },
+    });
 });
 
 app.listen(8000, '127.0.0.1', () => {
