a329f2e0bd4254a821600621d53dcd834c0be41e
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-16 12:03:19 +0430

    feat(firstify): Setup the TypeORM Module and DB connection

--- firstify/src/app.module.ts
+++ firstify/src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { CoffeeController } from './coffee/coffee.controller';
 import { CoffeeService } from './coffee/coffee.service';
 import { CoffeeModule } from './coffee/coffee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [
+    CoffeeModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'firstify',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CoffeeController],
   providers: [AppService, CoffeeService],
 })
