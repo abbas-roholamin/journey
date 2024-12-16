6d1e513920237725e4cf346fcb4fe118fa2f43eb
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-16 11:02:08 +0430

    feat(firstify): Validate Input Data with Data Transfer Objects

--- firstify/src/coffee/dto/update-coffee.dto.ts
+++ firstify/src/coffee/dto/update-coffee.dto.ts
@@ -1,5 +1,4 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/swagger';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
