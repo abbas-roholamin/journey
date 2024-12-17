9c133b4cc3e97a914c9312dc756fb89a973d5693
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-17 11:35:19 +0430

    feat(natours):  Chaining Multiple Middleware Functions

--- 04-building-the-natours-API/routes/TourRouter.js
+++ 04-building-the-natours-API/routes/TourRouter.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const TourController = require('../controllers/TourController');
 const ValidateTourIDMiddleware = require('../middleware/ValidateTourIDMiddleware');
+const CheckTourBodyMiddleware = require('../middleware/CheckTourBodyMiddleware');
 
 const router = express.Router();
 
 router.param('id', ValidateTourIDMiddleware);
 
 router.get('/api/v1/tours', TourController.getAllTours);
-router.post('/api/v1/tours', TourController.createTour);
+router.post('/api/v1/tours', CheckTourBodyMiddleware, TourController.createTour);
 router.patch('/api/v1/tours/:id', TourController.updateTour);
 router.get('/api/v1/tours/:id', TourController.getTour);
 router.delete('/api/v1/tours/:id', TourController.deleteTour);
