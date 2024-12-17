b97ef6b89809cb2290d10a61729df2bf90485c68
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-17 11:48:36 +0430

    feat(natours): Setup Environment Variables

--- 04-building-the-natours-API/app.js
+++ 04-building-the-natours-API/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
 
 const TourRouter = require('./routes/TourRouter');
 const UserRouter = require('./routes/UserRouter');
 
 const app = express();
 
+dotenv.config({ path: './config.env' });
+
 // Middleware
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
-app.use(morgan('dev'));
+
+if (process.env.MODE === 'dev') {
+    app.use(morgan('dev'));
+}
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
