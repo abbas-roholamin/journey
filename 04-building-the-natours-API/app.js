a3df17586791e4a7b2696b4169d98931f9620771
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-17 10:55:02 +0430

    feat(natours): Implementing the  Users  Routes

--- 04-building-the-natours-API/app.js
+++ 04-building-the-natours-API/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourController = require('./controllers/tourController');
+const TourController = require('./controllers/TourController');
+const UserController = require('./controllers/UserController');
 
 const app = express();
 app.use(express.json());
@@ -11,11 +12,17 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/api/v1/tours', tourController.getAllTours);
-app.post('/api/v1/tours', tourController.createTour);
-app.patch('/api/v1/tours/:id', tourController.updateTour);
-app.get('/api/v1/tours/:id', tourController.getTour);
-app.delete('/api/v1/tours/:id', tourController.deleteTour);
+app.get('/api/v1/tours', TourController.getAllTours);
+app.post('/api/v1/tours', TourController.createTour);
+app.patch('/api/v1/tours/:id', TourController.updateTour);
+app.get('/api/v1/tours/:id', TourController.getTour);
+app.delete('/api/v1/tours/:id', TourController.deleteTour);
+
+app.get('/api/v1/users', UserController.getAllUsers);
+app.post('/api/v1/users', UserController.createUser);
+app.patch('/api/v1/users/:id', UserController.updateUser);
+app.get('/api/v1/users/:id', UserController.getUser);
+app.delete('/api/v1/users/:id', UserController.deleteUser);
 
 app.listen(8000, '127.0.0.1', () => {
     console.log('Listening on http://127.0.0.1:8000');
