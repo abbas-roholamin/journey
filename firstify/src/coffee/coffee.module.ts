3ab89bd35e9036ad0454bd83b6ecaeabf7e0165e
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2024-12-21 21:30:47 +0430

    feat(firstify): Create a Relation between two Entities

--- firstify/src/coffee/coffee.module.ts
+++ firstify/src/coffee/coffee.module.ts
@@ -3,10 +3,11 @@ import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Falvor } from './entities/falvor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Falvor])],
   controllers: [CoffeeController],
   providers: [CoffeeService],
 })
-export class CoffeeModule {}
+export class CoffeeModule { }
--- firstify/src/coffee/entities/coffee.entity.ts
+++ firstify/src/coffee/entities/coffee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Falvor } from './falvor.entity';
 
 @Entity()
 export class Coffee {
@@ -11,6 +12,7 @@ export class Coffee {
   @Column()
   brand: string;
 
-  @Column('json', { nullable: true })
+  @JoinTable()
+  @ManyToMany(() => Falvor, (falvor) => falvor.coffees)
   flavors: string[];
 }
