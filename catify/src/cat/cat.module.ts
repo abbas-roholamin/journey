08f695901ba9f76b5f19dc7879b0d5e8ae504261
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-24 10:06:08 +0430

    feat(catify): Run linter to format code

--- catify/src/cat/cat.module.ts
+++ catify/src/cat/cat.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import {
+  //  Global,
+  Module,
+} from '@nestjs/common';
 import { CatService } from './cat.service';
 import { CatController } from './cat.controller';
 
+// @Global()
 @Module({
   controllers: [CatController],
   providers: [CatService],
+  exports: [
+    CatService,
+    //  CommonModule
+  ],
+  // imports: [CommonModule],
 })
-export class CatModule {}
+export class CatModule {
+  // constructor(private catsService: CatsService) {}
+}
