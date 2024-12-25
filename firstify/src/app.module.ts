ac582818d21f66979dbbb30e67c7c3fb70461800
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-25 11:11:31 +0430

    feat(firstify): Setup ENV file Schema Validation

--- firstify/src/app.module.ts
+++ firstify/src/app.module.ts
@@ -4,19 +4,27 @@ import { AppService } from './app.service';
 import { CoffeeModule } from './coffee/coffee.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       // ignoreEnvFile: false,
       // envFilePath: '.env.production',
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(3306),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_NAME: Joi.required(),
+      }),
     }),
     CoffeeModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DATABASE_HOST,
       port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
+      username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
