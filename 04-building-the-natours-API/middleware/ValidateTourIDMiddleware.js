93f7eb5b54616566fd228a3db5e9c9870592a8e5
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-25 08:56:21 +0430

    feat(natours): Run eslint to fix

--- 04-building-the-natours-API/middleware/ValidateTourIDMiddleware.js
+++ 04-building-the-natours-API/middleware/ValidateTourIDMiddleware.js
@@ -1,8 +1,6 @@
 module.exports = (req, res, next, val) => {
     const id = val * 1;
 
-    console.log(id);
-
     if (id <= 0) {
         return res.status(404).json({
             status: 'fail',
