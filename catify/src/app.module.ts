bdf019711ec148b8ca04b827229f2e5a211d9cf9
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-25 09:56:29 +0430

    feat(catify): Define custom middleware

--- catify/src/app.module.ts
+++ catify/src/app.module.ts
@@ -1,11 +1,39 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        // cors(),
+        // Helmet(),
+        LoggerMiddleware,
+      )
+      // .exclude(
+      //   { path: 'cats', method: RequestMethod.GET },
+      //   { path: 'cats', method: RequestMethod.POST },
+      //   'cats/(.*)',
+      // )
+      .forRoutes(
+        { path: 'cats', method: RequestMethod.GET },
+        // {
+        //   path: 'ab*cd',
+        //   method: RequestMethod.ALL,
+        // },
+        // CatController,
+      );
+  }
+}
