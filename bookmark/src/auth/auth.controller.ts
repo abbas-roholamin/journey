001b365075e520dfaccf4086131ee407c1de7a3a
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-28 10:05:37 +0430

    feat(bookmark): Create Auth services

--- bookmark/src/auth/auth.controller.ts
+++ bookmark/src/auth/auth.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import LoginDto from './dto/login.dto';
+import RegisterDto from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login() {
-    return this.authService.login();
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 
   @Post('register')
-  register() {
-    return this.authService.register();
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
   }
 
   @Post('logout')
--- bookmark/src/auth/auth.service.ts
+++ bookmark/src/auth/auth.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@nestjs/common';
-
+import LoginDto from './dto/login.dto';
+import RegisterDto from './dto/register.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
-  login() {
-    return 'login';
+  constructor(private prismaService: PrismaService) {}
+  login(loginDto: LoginDto) {
+    return loginDto;
   }
 
-  register() {
-    return 'register';
+  async register(registerDto: RegisterDto) {
+    const hash = await argon.hash(registerDto.password);
+
+    const newUser = await this.prismaService.user.create({
+      data: {
+        email: registerDto.email,
+        first_name: registerDto.first_name,
+        last_name: registerDto.last_name,
+        hash: hash,
+      },
+    });
+
+    return newUser;
   }
 
   logout() {
