275bf351c12c11557c1cc26cd3694a8ae8defc3f
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-29 10:03:23 +0430

    feat(firstify): Work with Swagger Module and CLI plugins

--- firstify/src/main.ts
+++ firstify/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { ApiKeyGuard } from './common/guard/api-key/api-key.guard';
 
 async function bootstrap() {
@@ -24,6 +25,16 @@ async function bootstrap() {
     new TimeoutInterceptor(),
   );
   // app.useGlobalGuards(new ApiKeyGuard());
+
+  const options = new DocumentBuilder()
+    .setTitle('Firstify')
+    .setDescription('The firstify API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
