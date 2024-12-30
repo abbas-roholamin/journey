72c71b147a3a63f269e488f7eca592454ad0f83a
Abbas Roholamin <abbasroholamin76@gmail.com>
2024-12-30 10:49:55 +0430

    feat(CFA): Create Graphql union type

--- code-first-approach/src/drinks/drinks.resolver.ts
+++ code-first-approach/src/drinks/drinks.resolver.ts
@@ -1,10 +1,21 @@
 import { Query, Resolver } from '@nestjs/graphql';
-import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { Coffee } from 'src/coffee/entities/coffee.entity/coffee.entity';
+// import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { DrinkResultUnion } from 'src/common/unions/drink-result.union';
+import { Tee } from 'src/tee/entities/tee.entity/tee.entity';
 
 @Resolver()
 export class DrinksResolver {
-  @Query(() => [Drink], { name: 'drinks' })
-  findAll(): Drink[] {
-    return [];
+  @Query(() => [DrinkResultUnion], { name: 'drinks' })
+  async findAll(): Promise<(typeof DrinkResultUnion)[]> {
+    const coffee = new Coffee();
+    coffee.id = 1;
+    coffee.name = 'Coffee 1';
+    coffee.brand = 'Brand 1';
+
+    const tee = new Tee();
+    tee.name = 'Tee 1';
+
+    return [coffee, tee];
   }
 }
