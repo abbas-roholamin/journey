f1dbc2fa5ad70130df23175bffb4991b1d4784a2
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-01 09:45:22 +0430

    feat(Natours): Implement tours CRUD operations

--- 04-building-the-natours-API/controllers/tourController.js
+++ 04-building-the-natours-API/controllers/tourController.js
@@ -1,50 +1,97 @@
-const fs = require('fs');
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+const Tour = require('../models/Tour');
 
 class TourController {
-    getTour(req, res) {
-        res.status(200).json({
-            status: 'success',
-            requestedAt: req.requestTime,
-            data: {
-                tour: 'The Park Camper',
-            },
-        });
+    async getTour(req, res) {
+        try {
+            const tour = await Tour.findById(req.params.id);
+
+            res.status(200).json({
+                status: 'success',
+                requestedAt: req.requestTime,
+                data: {
+                    tour,
+                },
+            });
+        } catch (error) {
+            res.status(404).json({
+                status: 'fail',
+                message: error.message,
+            });
+        }
     }
 
-    getAllTours(req, res) {
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tours,
-            },
-        });
+    async getAllTours(req, res) {
+        try {
+            const tours = await Tour.find();
+
+            res.status(200).json({
+                status: 'success',
+                results: tours.length,
+                data: {
+                    tours,
+                },
+            });
+        } catch (error) {
+            res.status(404).json({
+                status: 'fail',
+                message: error.message,
+            });
+        }
     }
 
-    createTour(req, res) {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: 'The Park Camper',
-            },
-        });
+    async createTour(req, res) {
+        try {
+            const tour = await Tour.create(req.body);
+
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour,
+                },
+            });
+        } catch (error) {
+            res.status(400).json({
+                status: 'fail',
+                message: error.message,
+            });
+        }
     }
 
-    updateTour(req, res) {
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour: 'The Park Camper',
-            },
-        });
+    async updateTour(req, res) {
+        try {
+            const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+                runValidators: true,
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    tour,
+                },
+            });
+        } catch (error) {
+            res.status(404).json({
+                status: 'fail',
+                message: error.message,
+            });
+        }
     }
 
-    deleteTour(req, res) {
-        res.status(204).json({
-            status: 'success',
-            data: null,
-        });
+    async deleteTour(req, res) {
+        try {
+            await Tour.findByIdAndDelete(req.params.id);
+
+            res.status(204).json({
+                status: 'success',
+                data: null,
+            });
+        } catch (error) {
+            res.status(404).json({
+                status: 'fail',
+                message: error.message,
+            });
+        }
     }
 }
 
