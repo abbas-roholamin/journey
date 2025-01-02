3650ccf5868770fe6207300722e08869bc5b8216
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-02 11:02:37 +0430

    feat(Natours): Modelling the Tours model

--- 04-building-the-natours-API/models/Tour.js
+++ 04-building-the-natours-API/models/Tour.js
@@ -5,10 +5,23 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
+        trim: true,
     },
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size'],
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
     },
     difficulty: {
         type: String,
@@ -18,14 +31,40 @@ const tourSchema = new mongoose.Schema({
             message: 'Difficulty is either: easy, medium, difficult',
         },
     },
-    ratings: {
-        type: Number,
-        default: 4.5,
-    },
     price: {
         type: Number,
         required: [true, 'A tour must have a price'],
     },
+    discount: {
+        type: Number,
+        validate: {
+            validator: function (val) {
+                return val < this.price;
+            },
+            message: 'Discount price ({VALUE}) should be below regular price',
+        },
+    },
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description'],
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description'],
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false,
+    },
+    startDates: [Date],
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
