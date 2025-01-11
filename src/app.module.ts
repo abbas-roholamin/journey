b3e37147e25dbc10610249a51b7e8d1f322c22ff
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-11 10:07:30 +0430

    feat: Setup and config database connection
    1- Install and config the config module
    2- Install and config the Typeorm to connect with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.APP_ENV == 'development',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
