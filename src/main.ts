54619032b3cbcca720042353301e5e9e2ef10ead
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-12 08:44:44 +0430

    feat: Enable CORS for all origin

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
