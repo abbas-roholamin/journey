079ca8668690e7580f33ab381e74a08545f355f7
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-14 11:45:19 +0430

    feat(Natours): Making the API Better with top tours middleware

--- 04-building-the-natours-API/routes/TourRouter.js
+++ 04-building-the-natours-API/routes/TourRouter.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const TourController = require('../controllers/TourController');
 const ValidateTourIDMiddleware = require('../middleware/ValidateTourIDMiddleware');
+const TopToursMiddleware = require('../middleware/TopToursMiddleware');
 
 const router = express.Router();
 
 router.param('id', ValidateTourIDMiddleware);
 
+router.get('/api/v1/tours/top-tours', TopToursMiddleware, TourController.getAllTours);
 router.get('/api/v1/tours', TourController.getAllTours);
 router.post('/api/v1/tours', TourController.createTour);
 router.patch('/api/v1/tours/:id', TourController.updateTour);
