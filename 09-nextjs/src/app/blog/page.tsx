ab7652f599a9731145a17b1067393aeeec7323d6
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-16 10:02:59 +0430

    feat(Nextjs): Work with title metadata

--- 09-nextjs/src/app/blog/page.tsx
+++ 09-nextjs/src/app/blog/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog Page",
+  },
+};
+
 function Page() {
-    return (
-      <div>Blog Page</div>
-    )
-  }
-  
-  export default Page
+  return <div>Blog Page</div>;
+}
+
+export default Page;
