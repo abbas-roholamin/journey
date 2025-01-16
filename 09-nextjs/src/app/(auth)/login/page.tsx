d8005593539dd103a031f5377c779a72d7b993ca
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-16 09:59:42 +0430

    feat(Nextjs): Work with Routing Metadata

--- 09-nextjs/src/app/(auth)/login/page.tsx
+++ 09-nextjs/src/app/(auth)/login/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Login page",
+};
+
 function Page() {
   return <div>Login</div>;
 }
--- 09-nextjs/src/app/products/[productId]/page.tsx
+++ 09-nextjs/src/app/products/[productId]/page.tsx
@@ -1,13 +1,24 @@
+import { Metadata } from "next";
 type Props = {
-  params: Promise<{productId: string}>
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const productId = (await params).productId;
+  const title = await new Promise((resolve) =>
+    setTimeout(() => resolve(`The product ${productId}`), 1000)
+  );
+  return {
+    title: ` ${title} details Page`,
+  };
+};
+
+async function Page({ params }: Props) {
+  const producId = (await params).productId;
+
+  return <p>Product {producId} details Page</p>;
 }
 
-async function Page({params}: Props) {
-  const producId = (await params).productId
-
-  return (
-    <p>Product {producId} details Page</p>
-  )
-}
-
-export default Page
+export default Page;
