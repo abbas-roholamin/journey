41189095f445eae04fa4c5bd4f99d8f4e0161c6b
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-21 08:43:27 +0430

    feat(Nextjs): Work with Recovering from Errors

--- 09-nextjs/src/app/products/[productId]/error.tsx
+++ 09-nextjs/src/app/products/[productId]/error.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-function error({ error }: { error: Error }) {
-  return <div className="text-red-500">{error.message}</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+function error({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
+  return (
+    <div className="text-red-500">
+      <p>{error.message}</p>
+      <button onClick={() => reload()}>Try Again</button>
+    </div>
+  );
 }
 
 export default error;
