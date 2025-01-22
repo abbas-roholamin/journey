f156974037c8b7e460dd1bb9a05172acc35d0143
Abbas Roholamin <abbasroholamin76@gmail.com>
2025-01-22 11:56:50 +0430

    feat(Natours): Work with Virtual Properties

--- 04-building-the-natours-API/models/Tour.js
+++ 04-building-the-natours-API/models/Tour.js
@@ -1,70 +1,80 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-        trim: true,
-    },
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration'],
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size'],
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5,
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0,
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty'],
-        enum: {
-            values: ['easy', 'medium', 'difficult'],
-            message: 'Difficulty is either: easy, medium, difficult',
+const tourSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+            trim: true,
         },
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price'],
-    },
-    discount: {
-        type: Number,
-        validate: {
-            validator: function (val) {
-                return val < this.price;
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A tour must have a group size'],
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 4.5,
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0,
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A tour must have a difficulty'],
+            enum: {
+                values: ['easy', 'medium', 'difficult'],
+                message: 'Difficulty is either: easy, medium, difficult',
             },
-            message: 'Discount price ({VALUE}) should be below regular price',
         },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price'],
+        },
+        discount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    return val < this.price;
+                },
+                message: 'Discount price ({VALUE}) should be below regular price',
+            },
+        },
+        summary: {
+            type: String,
+            trim: true,
+            required: [true, 'A tour must have a description'],
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: [true, 'A tour must have a description'],
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have a cover image'],
+        },
+        images: [String],
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            select: false,
+        },
+        startDates: [Date],
     },
-    summary: {
-        type: String,
-        trim: true,
-        required: [true, 'A tour must have a description'],
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
     },
-    description: {
-        type: String,
-        trim: true,
-        required: [true, 'A tour must have a description'],
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image'],
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false,
-    },
-    startDates: [Date],
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+    return Math.round(this.duration / 7);
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
