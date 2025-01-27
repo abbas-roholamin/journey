9a218f56454c1aba8c8a40716b2fcb0e1e9bf1a7
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-01-27 17:42:23 +0430

    feat(angular): work with event and property binding

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular';
+  title = signal('Hello, world');
+  image_url = signal('https://placehold.co/600x400');
+
+  getName() {
+    return this.title().toUpperCase();
+  }
+
+  updateImage(e: KeyboardEvent) {
+    this.image_url.set((e.target as HTMLInputElement).value);
+  }
 }
