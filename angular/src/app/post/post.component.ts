377d59ea9caa5c2c7006c3b33d696e1d9dd673c9
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-01-28 20:11:24 +0430

    feat(Angular): Work with lifecycle hooks

--- angular/src/app/post/post.component.ts
+++ angular/src/app/post/post.component.ts
@@ -1,4 +1,17 @@
-import { Component, input, output } from '@angular/core';
+import {
+  Component,
+  input,
+  output,
+  OnInit,
+  DoCheck,
+  OnChanges,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+  AfterContentChecked,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -6,9 +19,54 @@ import { Component, input, output } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.css',
 })
-export class PostComponent {
+export class PostComponent
+  implements
+    OnInit,
+    DoCheck,
+    OnChanges,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   title = input.required();
   url = input.required();
 
   callDad = output<string>();
+
+  constructor() {
+    console.log('ehllo');
+  }
+
+  ngOnInit() {
+    console.log('init');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('afterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('afterContentInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnChanges');
+  }
 }
