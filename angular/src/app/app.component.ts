2e90751769acf45a4e19abf788b128b52da0f339
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-01-28 22:30:13 +0430

    feat(Angular): Practice some new Pipe

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from './post/post.component';
-import { TitleCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [PostComponent, TitleCasePipe],
+  imports: [PostComponent, TitleCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = signal('hello, world');
   image_url = signal('https://placehold.co/600x400');
+  date = signal(new Date());
+  cost = signal(12000);
 
   getName() {
     return this.title().toUpperCase();
