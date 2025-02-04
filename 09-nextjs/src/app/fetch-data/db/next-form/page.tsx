0b9b3a4248d5cc9cd5fd4726039c641169364157
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-04 10:19:09 +0430

    feat(Nextjs): Implement DB create operation with server action

---
+++ 09-nextjs/src/app/fetch-data/db/next-form/page.tsx
@@ -0,0 +1,42 @@
+import { createProduct } from "@/prisma-db";
+import { redirect } from "next/navigation";
+
+export default function CreateProductForm() {
+  async function create(formData: FormData) {
+    "use server";
+    const data = {
+      title: formData.get("title") as string,
+      description: formData.get("description") as string,
+      price: parseInt(formData.get("price") as string),
+      qauntity: parseInt(formData.get("qauntity") as string),
+    };
+
+    await createProduct(data);
+    redirect("/fetch-data/db");
+  }
+
+  return (
+    <div className="grid place-content-center h-full ">
+      <form action={create} className="flex flex-col gap-4 text-black ">
+        <label>
+          <input type="text" name="title" />
+        </label>
+        <label>
+          <textarea name="description" />
+        </label>
+        <label>
+          <input type="number" name="price" />
+        </label>
+        <label>
+          <input type="number" name="qauntity" />
+        </label>
+        <button
+          type="submit"
+          className="p-4 rounded bg-white text-black disabled:animate-pulse"
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
+}
