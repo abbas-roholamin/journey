ab57775ba51a2e4bbc172581d30c2509237d1cfd
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-04 11:16:26 +0430

    feat(Typescript): Creating Typed Map

---
+++ typescript/src/map.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+type User = {
+  id: number
+  name: string
+}
+
+const numbers = new Map<number, User>()
+const oddNumber: Map<number, User> = new Map()
+const evenNumber: Map<number, User> = new Map([
+  [2, { id: 1, name: "abbas" }],
+  [4, { id: 2, name: "Ali" }],
+  [6, { id: 3, name: "Reza" }],
+])
+
+numbers.set(1, { id: 1, name: "abbas" })
+oddNumber.set(1, { id: 1, name: "abbas" })
+evenNumber.set(10, { id: 1, name: "abbas" })
+
+// @ts-expect-error
+numbers.set("", "hello")
+// @ts-expect-error
+oddNumber.set("", "hello")
+// @ts-expect-error
+evenNumber.set(1, "hello")
