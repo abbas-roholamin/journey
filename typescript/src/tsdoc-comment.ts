efe61d7bd3e63a75cd2188719f70251c5f856937
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-05 12:31:35 +0430

    feat(Typescript): Define TSDOC comments

---
+++ typescript/src/tsdoc-comment.ts
@@ -0,0 +1,22 @@
+/** @format */
+
+/**
+
+ * Add two numbers.
+ *
+ * @param x The first number.
+ * @param y The second number.
+ * @returns The sum of x and y.
+ *
+ * @throws Will throw an error if x or y is not a number.
+ *
+ * @example
+ * const sum = add(1, 2);
+ *
+ *
+ */
+function add(x: number, y: number): number {
+  return x + y
+}
+
+add(1, 2)
