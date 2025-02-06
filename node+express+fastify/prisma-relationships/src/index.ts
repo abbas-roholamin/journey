ec8de3be1c1fe185c0a2c868b463f819b03ffbf8
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-06 14:50:54 +0430

    feat(Node):  Setup Prisma client and Express

---
+++ node+express+fastify/prisma-relationships/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import UserController from './controllers/user.controller'
+import { prisma } from '../prisma.db'
+import postController from './controllers/post.controller'
+
+async function main() {
+    const app = express()
+
+    app.use(express.json())
+
+    // User Routes
+
+    app.listen(8800, '127.0.0.1', () => {
+        console.log('Listening on http://127.0.0.1:8800')
+    })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
