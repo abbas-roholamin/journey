9bd5dae43abdeb5df69b9f153a1a46c712cd1f42
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-08 09:57:55 +0430

    feat(Typescript):Throwing Errors to Narrow

--- javascript+typescript/total-typescript-typescript-pro-sssentials/src/dom/index.ts
+++ javascript+typescript/total-typescript-typescript-pro-sssentials/src/dom/index.ts
@@ -1,2 +1,9 @@
 /** @format */
 export const getElementByIdElement = document.getElementById("my-element")
+
+// Throw error for narrowing
+
+export const element = document.getElementById("my-element")
+
+if (!element) throw new Error("Element not found")
+console.log(element)
