6ec2ec70e332442b95e367667cb18ebd80c75314
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-10 14:33:09 +0430

    feat(Typescript):Extending Object with Interface and Type

--- javascript+typescript/total-typescript-typescript-pro-sssentials/src/typing-object-literals.ts
+++ javascript+typescript/total-typescript-typescript-pro-sssentials/src/typing-object-literals.ts
@@ -15,3 +15,63 @@ function concatName(user: User): string {
 }
 
 concatName(user)
+
+// Extending Object with Type
+type TBaseEntity = {
+  id: number
+  created_at: Date
+}
+
+type TCustomer = {
+  name: string
+  email: string
+} & TBaseEntity
+
+type TProduct = {
+  name: string
+  price: number
+} & TBaseEntity
+
+export const type_customer: TCustomer = {
+  id: 1,
+  name: "abbas",
+  email: "abbas@.com",
+  created_at: new Date(),
+}
+
+export const type_product: TProduct = {
+  id: 1,
+  name: "abbas",
+  price: 100,
+  created_at: new Date(),
+}
+
+// Extending Object with Interface
+interface IBaseEntity {
+  id: number
+  created_at: Date
+}
+
+interface ICustomer extends IBaseEntity {
+  name: string
+  email: string
+}
+
+interface IProduct extends IBaseEntity {
+  name: string
+  price: number
+}
+
+export const interface_customer: ICustomer = {
+  id: 1,
+  name: "abbas",
+  email: "abbas@.com",
+  created_at: new Date(),
+}
+
+export const interface_product: IProduct = {
+  id: 1,
+  name: "abbas",
+  price: 100,
+  created_at: new Date(),
+}
