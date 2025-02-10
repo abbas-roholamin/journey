487f6d6037d6d57645855484a6a294bdaf2671e8
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-02-10 10:14:35 +0430

    feat(Next): Read Session and User Data

--- react+next/codevolution-nextjs/src/app/client-only/page.tsx
+++ react+next/codevolution-nextjs/src/app/client-only/page.tsx
@@ -2,11 +2,27 @@
 
 import { ClientOnly } from "../utils/client-only";
 // import { serverOnly } from "../utils/server-onyl";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 function Page() {
   // const data = serverOnly(); Error
   const data = ClientOnly();
-  return <div>Profile Page {data}</div>;
+  const { isLoaded, isSignedIn } = useAuth();
+  const { user } = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return <div>user not signed in</div>;
+  }
+
+  if (!user) {
+    return <div>user not found</div>;
+  }
+
+  return (
+    <div>
+      Profile Page {data} - {user.id}
+    </div>
+  );
 }
 
 export default Page;
--- react+next/codevolution-nextjs/src/app/dashboard/page.tsx
+++ react+next/codevolution-nextjs/src/app/dashboard/page.tsx
@@ -1,5 +1,14 @@
-function Page() {
-  return <div>Dashboard</div>;
+import { auth, currentUser } from "@clerk/nextjs/server";
+async function Page() {
+  const authObj = await auth();
+  const userObj = await currentUser();
+  return (
+    <div>
+      Dashboard
+      <div>{JSON.stringify(authObj)}</div>
+      <div>{JSON.stringify(userObj)}</div>
+    </div>
+  );
 }
 
 export default Page;
