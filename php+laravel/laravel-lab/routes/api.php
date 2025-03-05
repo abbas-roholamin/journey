72f67faebd0e9fce5c743121ef8c2e6eff909b98
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-03-05 08:52:56 +0430

    feat(Laravel): Supercharge APIs with whenLoaded

---
+++ php+laravel/laravel-lab/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
