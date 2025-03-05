1293480c44bbcc5cbfbdcc1fa09b6cd95dff6d0b
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-03-05 13:43:42 +0430

    feat(Laravel): Work with whenLoaded in resource

--- php+laravel/laravel-lab/routes/api.php
+++ php+laravel/laravel-lab/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Resources\PostResource;
 use App\Http\Resources\UserResource;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +11,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/posts-with-user', function () {
+    return PostResource::collection(Post::with(relations: "user")->get());
+});
