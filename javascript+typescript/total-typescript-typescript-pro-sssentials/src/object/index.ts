8a828fea13452ffdc2055fd8bfb92861cdfb860c
abbas-roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2025-03-12 06:55:06 +0430

    feat(Typescript):  Inference diff between let and const

--- javascript+typescript/total-typescript-typescript-pro-sssentials/src/object/index.ts
+++ javascript+typescript/total-typescript-typescript-pro-sssentials/src/object/index.ts
@@ -9,3 +9,16 @@ type Routes = (typeof routes)[keyof typeof routes]
 export function getRoute(url: Routes): string {
   return url
 }
+
+// Inference diff between let and const
+
+type ButtonAttribute = {
+  type: "button" | "submit" | "reset"
+}
+
+// let type = "button"
+const type = "button"
+
+export const buttonAttribute: ButtonAttribute = {
+  type,
+}
